c4fedd1118a2740a8b4570149f0bf2a5b846bbbf
Black - BKP <black.bkp@gmail.com>
2021-08-05 09:38:44 +0700

    Add Invoice to Progress Model

--- LaborCostAnalysis/Models/ProgressModel.cs
+++ LaborCostAnalysis/Models/ProgressModel.cs
@@ -8,23 +8,15 @@ namespace LaborCostAnalysis.Models
     public class ProgressModel
     {
         public string job_id { get; set; }
-
         public string job_number { get; set; }
-
         public string job_name { get; set; }
-
         public int year { get; set; }
-
         public int month { get; set; }
-
         public int estimated_budget { get; set; }
-
         public int job_progress { get; set; }
-
         public int spent_cost { get; set; }
-
         public int spent_cost_acc { get; set; }
-
         public int remainning_cost { get; set; }
+        public int invoice { get; set; }
     }
 }
